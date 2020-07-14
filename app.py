0897ff99604211795c6c8bb8917e269f29b1a428
Reelshrimpboat <Camneelycreative@gmail.com>
2020-07-14 14:52:42 -0500

    post functional with JSON object in post

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import  request, jsonify
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 import psycopg2
 
@@ -31,33 +30,39 @@ def home():
 
 
 # A route to return all of the available entries in our catalog.
-@app.route('/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+@app.route('/books', methods=['GET', 'POST'])
+def book_route():
+    if request.method == 'GET':
+        return jsonify(books)
+    elif request.method == 'POST':
+        book = request.get_json()
+        print("book: ", book)
+        books.append(book)
+        return {'id': len(books)}, 200
 
+# # GET that calls for a single book
+# @app.route('/books', methods=['GET'])
+# def api_id():
+#     # Check if an ID was provided as part of the URL.
+#     # If ID is provided, assign it to a variable.
+#     # If no ID is provided, display an error in the browser.
+#     if 'id' in request.args:
+#         id = int(request.args['id'])
+#     else:
+#         return "Error: No id field provided. Please specify an id."
 
-@app.route('/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+#     # Create an empty list for our results
+#     results = []
 
-    # Create an empty list for our results
-    results = []
+#     # Loop through the data and match results that fit the requested ID.
+#     # IDs are unique, but other fields might return many results
+#     for book in books:
+#         if book['id'] == id:
+#             results.append(book)
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+#     # Use the jsonify function from Flask to convert our list of
+#     # Python dictionaries to the JSON format.
+#     return jsonify(results)
 
 
 
