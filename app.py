974c22ec6927c9d744ed728cff511189fe17d9d4
Reelshrimpboat <Camneelycreative@gmail.com>
2020-07-14 14:10:29 -0500

    splats to DOM as a JSON

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
-import psycopg2
 from flask import Flask
 app = Flask(__name__)
+from flask import jsonify
+import psycopg2
 
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    try:
+        connection = psycopg2.connect(
+                                        host="localhost",
+                                        port="5432",
+                                        database="garden")
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "select * from plant"
 
+        cursor.execute(postgreSQL_select_Query)
+        print("Selecting rows from plants")
+        plants = cursor.fetchall()
 
-try:
-   connection = psycopg2.connect(
-                                 host="localhost",
-                                 port="5432",
-                                 database="garden")
-   cursor = connection.cursor()
-   postgreSQL_select_Query = "select * from plant"
+        print("Print each row and it's columns values")
+        list = []
+        for row in plants:
+            list.append(row[1])
+            # print("id = ", row[0], )
+            # print("plant = ", row[1])
+        print("list = ", list)
+        return jsonify(list)
 
-   cursor.execute(postgreSQL_select_Query)
-   print("Selecting rows from plants")
-   plants = cursor.fetchall()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
 
-   print("Print each row and it's columns values")
-   for row in plants:
-       print("id = ", row[0], )
-       print("plant = ", row[1])
-
-except (Exception, psycopg2.Error) as error:
-    print("Error while fetching data from PostgreSQL", error)
-
-finally:
-    #closing database connection.
-    if(connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
